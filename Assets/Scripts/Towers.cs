2fceec284981427e5f5ac80467913436b0c9c5be
okayclay <53243950+okayclay@users.noreply.github.com>
2020-06-01 21:58:46 -0400

    Script update
    Getting the scripts started:
    Will need an enemy class
    Made the tower class, a child of draggable

---
+++ Assets/Scripts/Towers.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Towers : Draggable
+{
+    public enum ElementTypes
+    {
+        None,   //Default
+        Fire,
+        Electric,
+        Ice,
+        Biological
+    }
+
+    [SerializeField] protected int      m_level;        //Related to ammo type
+    [SerializeField] protected float    m_damage;       //Higher the level, higher the damage
+    [SerializeField] protected int      m_cost;         //Higher cost too
+    [SerializeField] protected int      m_lineOfSight;  //Maybe larger line of sight, idk
+
+    protected Enemy m_closestEnemy; //The target
+
+    /// <summary>
+    /// Make an invisible sphere and anything that walks in gets shot
+    /// </summary>
+    protected void CheckDistanceFromEnemy()
+    {
+        Collider[] collisions = Physics.OverlapSphere(transform.position, m_lineOfSight);
+        foreach(Collider collider in collisions)
+        {
+            switch(collider.tag)
+            {
+                case "Enemy":
+                    m_closestEnemy = collider.GetComponent<Enemy>();
+                    break;
+            }
+        }
+    }
+
+    protected void FixedUpdate()
+    {
+        CheckDistanceFromEnemy();
+    }
+    
+    /// <summary>
+    /// Debug. Shows the sphere youre working with
+    /// </summary>
+    protected void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawSphere(transform.position, m_lineOfSight);
+    }
+}
