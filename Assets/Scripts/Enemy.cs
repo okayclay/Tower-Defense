a95fefb07094e484683d27f6cc922ee65ba42c00
okayclay <53243950+okayclay@users.noreply.github.com>
2020-06-04 17:00:51 -0400

    Added Characters
    Added the zombie and Mummy enemy
    Set up the animation controller
    Added box colliders and the enemy script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    Animator m_anim;
+
+    [SerializeField] protected float m_startingHealth;
+
+    protected float m_curHealth;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_anim = GetComponent<Animator>();
+        m_curHealth = m_startingHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            m_anim.SetBool("Walk", true);
+        }
     }
 }
