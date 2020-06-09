2adbae5b219439bce222b35e3cf0e28e4559007b
okayclay <53243950+okayclay@users.noreply.github.com>
2020-06-09 16:08:56 -0400

    Testing NavMesh
    Added NavMesh Agents to enemy prefabs
    Added code to make it run towards a certain spot
    Added the spot to testing level

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
@@ -8,21 +9,27 @@ public class Enemy : MonoBehaviour
 
     [SerializeField] protected float m_startingHealth;
 
-    protected float m_curHealth;
+    protected float         m_curHealth;
+    protected NavMeshAgent  m_agent;
+
+    void BeginWalking()
+    {
+        m_anim.SetBool("Run", true);
+        m_agent.SetDestination(LevelManager.endPoint.position);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         m_anim = GetComponent<Animator>();
         m_curHealth = m_startingHealth;
+        m_agent = GetComponent<NavMeshAgent>();
+
+        BeginWalking();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.J))
-        {
-            m_anim.SetBool("Walk", true);
-        }
     }
 }
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    public static Transform endPoint;
+
     // Start is called before the first frame update
     void Start()
     {
+        endPoint = transform.Find("Goal");
+
+        if (endPoint == null)
+            Debug.LogWarning("Couldnt find the destination for this level");
     }
 
     // Update is called once per frame
