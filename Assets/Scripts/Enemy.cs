2f96db60393421ab2fc4737ea27aa0126aacb267
okayclay <53243950+okayclay@users.noreply.github.com>
2020-06-09 19:42:14 -0400

    Move enum for Enemy
    When destination is reached switch to attack mode

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,17 +5,60 @@ using System.Collections.Generic;
 
 public class Enemy : MonoBehaviour
 {
-    Animator m_anim;
+    enum Mode
+    {
+        None,
+        Move,
+        Attack
+    }
+
+    Animator    m_anim;
+    Mode        m_mode = Mode.None;
 
     [SerializeField] protected float m_startingHealth;
 
     protected float         m_curHealth;
     protected NavMeshAgent  m_agent;
+    protected bool          m_pathSet = false;
 
     void BeginWalking()
     {
-        m_anim.SetBool("Run", true);
-        m_agent.SetDestination(LevelManager.EndPoint.position);
+        ChangeMode(Mode.Move);
+        m_pathSet = true;
+    }
+
+    void ChangeMode(Mode newMode)
+    {
+        m_mode = newMode;
+
+        switch(newMode)
+        {
+            case Mode.Move:
+                m_anim.SetBool("Run", true);
+                m_agent.SetDestination(LevelManager.EndPoint.position);
+                break;
+
+            case Mode.Attack:
+                m_anim.SetBool("Run", false);
+                m_anim.SetBool("Attack", true);
+                break;
+        }
+    }
+
+    bool FinishedPath()
+    {
+        if (!m_agent.pathPending && m_pathSet)   //no longer making the path
+        {
+            if (m_agent.remainingDistance <= m_agent.stoppingDistance)  //at stopping distance
+            {
+                if (!m_agent.hasPath || m_agent.velocity.sqrMagnitude == 0f)    //no more path because you made it to the end
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
     // Start is called before the first frame update
@@ -32,7 +75,12 @@ public class Enemy : MonoBehaviour
 
     private void Update()
     {
-        
-        //m_anim.SetBool("Run", false);
+        switch(m_mode)
+        {
+            case Mode.Move:
+                if (FinishedPath())
+                    ChangeMode(Mode.Attack);
+                break;
+        }
     }
 }
